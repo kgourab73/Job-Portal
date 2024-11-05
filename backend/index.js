ba88bd3be20433a074bda940c89f123ca8ac7275
Kirtigourab <kggouda732003@gmail.com>
2024-11-06 00:23:57 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://job-portal-1cjo.onrender.com",
   credentials: true,
 };
 app.use(cors(corsOptions));
